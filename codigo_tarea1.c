fa282f17d9209bde62413719af44d60071c70b35
CataCordova <80425504+CataCordova@users.noreply.github.com>
2021-03-18 23:24:03 -0300

    Entrar

--- codigo_tarea1.c
+++ codigo_tarea1.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
+
+const char *getfield(char *line, int num)
+{
+    const char *tok;
+    for (tok = strtok(line, ";");
+         tok && tok;
+         tok = strtok(NULL, ";\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
 
 int main()
 {
-    printf("Todas las notas deben estar en decimal");
-    FILE *archivo
-        archivo = fdopen("tarea1 notas.csv", "r");
+    FILE stream = fopen("notas.csv", "r");
+
+    char line[1024];
+    while (fgets(line, 1024, stream))
+    {
+        char *tmp = strdup(line);
+        printf("Field 3 would be %s\n", getfield(tmp, 3));
+        // NOTE strtok clobbers tmp
+        free(tmp);
+    }
 }
