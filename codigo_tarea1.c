f00e10949adab7f45e32665c3c9840df34d2b20c
CataCordova <80425504+CataCordova@users.noreply.github.com>
2021-03-18 22:51:52 -0300

    Entrar

---
+++ codigo_tarea1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    printf("Todas las notas deben estar en decimal");
+    FILE *archivo
+        archivo = fdopen("tarea1 notas.csv", "r");
+}
